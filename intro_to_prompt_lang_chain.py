9bf93ec29115ac36ee060baa2489f8c3d6bd15de
Joseph Adewunmi <adedejiosvaldo@gmail.com>
2025-01-29 15:11:42 +0100

    Feature -> Optimizied the code

--- intro_to_prompt_lang_chain.py
+++ intro_to_prompt_lang_chain.py
@@ -17,18 +17,28 @@ model = AzureChatOpenAI(
 )
 
 
-customer_review = ""
-template_string = f"""
-    Rewrite {customer_review} in a more polite tone and then translate
-    the new review message into spanish
-    """
+# customer_review = ""
+# company_name = ""
+# product_name = ""
+# template_string = f"""
+#     Rewrite {customer_review} in a more polite tone and then translate
+#     the new review message into spanish. The company name is {company_name}
+#     and make sure to mention the product name {product_name}
+#     """
 
 
 # Define the function to get completion
-def get_completion():
+def get_completion(customer_review: str, company_name: str, product_name: str):
+    template_string = f"""
+    Please rewrite the following review in a more polite and professional tone, making sure to mention the company name {company_name} and the product name {product_name}:
+    "{customer_review}"
+    Then, translate the revised review into Spanish.
+    """
     prompt_template = ChatPromptTemplate.from_template(template_string)
     translation_message = prompt_template.format_messages(
-        customer_review=customer_review
+        customer_review=customer_review,
+        company_name=company_name,
+        product_name=product_name,
     )
     response = model.invoke(translation_message)
     return response.content
@@ -40,8 +50,9 @@ def get_completion():
 
 if __name__ == "__main__":
     customer_review = input("Enter the review: ")
-
+    company_name = input("Enter the company name: ")
+    product_name = input("Enter the product name: ")
     print("_____________Loading_____________")
     # Get the completion
-    print(get_completion())
+    print(print(get_completion(customer_review, company_name, product_name)))
     # print(rewrite)
